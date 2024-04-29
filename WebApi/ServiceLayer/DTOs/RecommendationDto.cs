fd523db81f9be8fb4ff6faf451efe8e709938845
gpcodeac <pashkovskij@gmail.com>
2024-04-29 22:35:31 +0300

    recommendations first algorithm done

---
+++ WebApi/ServiceLayer/DTOs/RecommendationDto.cs
@@ -0,0 +1,13 @@
+﻿using WebApi.DataAccessLayer.Models;
+
+namespace WebApi.ServiceLayer.DTOs
+{
+    public class RecommendationDto
+    {
+        public List<BookModel> RecommendedByCosineDistance { get; set; } = new List<BookModel>();
+
+        public List<BookModel> RecommendedByMatrixFactorization { get; set; } = new List<BookModel>();
+
+        public List<BookModel> RecommendedByFieldAwareMatrixF { get; set; } = new List<BookModel>();
+    }
+}
--- WebApi/ServiceLayer/Interfaces/IUserService.cs
+++ WebApi/ServiceLayer/Interfaces/IUserService.cs
@@ -1,6 +1,8 @@
-﻿namespace WebApi.ServiceLayer;
+﻿using WebApi.DataAccessLayer.Models;
+
+namespace WebApi.ServiceLayer;
 
 public interface IUserService
 {
-    
+    ICollection<ReviewModel> GetReviews(string username);
 }
