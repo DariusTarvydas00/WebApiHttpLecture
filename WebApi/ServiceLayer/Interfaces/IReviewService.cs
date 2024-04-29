4e129b338905b3d63d89ced1aed5d793e5ed24ff
MigleKi <149431653+MigleKi@users.noreply.github.com>
2024-04-29 21:09:08 +0300

    Added Review services and repository

--- WebApi/ServiceLayer/Interfaces/IReviewService.cs
+++ WebApi/ServiceLayer/Interfaces/IReviewService.cs
@@ -1,6 +1,10 @@
-﻿namespace WebApi.ServiceLayer;
+﻿using WebApi.DataAccessLayer.Models;
+
+namespace WebApi.ServiceLayer;
 
 public interface IReviewService
 {
-    
+    ReviewModel CreateNewReview(ReviewModel review);
+    List<ReviewModel> GetAllReviewsByBookId(int bookId);
+    public ReviewModel RemoveReview(int reviewId);
 }
