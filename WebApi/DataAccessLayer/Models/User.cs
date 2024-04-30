23d88b78310b5184bddf841adc798ed416ce02b6
gpcodeac <pashkovskij@gmail.com>
2024-04-30 22:06:41 +0300

    first mods

--- WebApi/DataAccessLayer/Models/User.cs
+++ WebApi/DataAccessLayer/Models/User.cs
@@ -24,6 +24,10 @@ namespace WebApi.DataAccessLayer.Models
 
         [StringLength(50)]
         public string? Role { get; set; }
+        public string? Location { get; set; }
+
+        [StringLength(3)]
+        public string? Age { get; set; }
 
         public virtual ICollection<Review> Reviews { get; set; } = new HashSet<Review>();
     }
